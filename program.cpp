7d4aa4ab4ee5483e34143649004699f307e1eb37
Pratyush Shukla <33959192+hawk-praxs@users.noreply.github.com>
2019-04-24 22:00:46 +0530

    Optimised the code
    Code follows naming conventions of C++. OOPS properly implemented. Optimisation on speed.

--- program.cpp
+++ program.cpp
@@ -1,197 +1,192 @@
-// CPP program to implement preemptive priority scheduling 
 #include <bits/stdc++.h> 
 using namespace std; 
-  
-struct Process { 
-    int processID; 
-    int burstTime; 
-    int tempburstTime; 
-    int responsetime; 
-    int arrivalTime; 
+
+public class Process
+{   
+    // Process and its attributes
+    public:
+    int process_id; 
+    int burst_time; 
+    int temp_burst_time; 
+    int response_time; 
+    int arrival_time; 
     int priority; 
-    int outtime; 
-    int intime; 
-}; 
+    int out_time; 
+    int in_time;
+
+    private:
+    // Insert processes for execution in the ready queue
+    // queueSize defines the number of processes in execution depending on the currentTime variable
+    // currentTime keeps a record of the current CPU time. 
+    void insert(Process Queue[], Process value, int* queue_size, int* current_time)  
+    { 
+      int start = *queue_size, i; 
+      Queue[*queue_size] = value; 
+      if (Queue[*queue_size].intime == -1) 
+          Queue[*queue_size].intime = *current_time; 
+      ++(*queue_size); 
   
-// It is used to include all the valid and eligible 
-// processes in the heap for execution. heapsize defines 
-// the number of processes in execution depending on 
-// the current time currentTime keeps a record of 
-// the current CPU time. 
-void insert(Process Heap[], Process value, int* heapsize, 
-            int* currentTime) 
-{ 
-    int start = *heapsize, i; 
-    Heap[*heapsize] = value; 
-    if (Heap[*heapsize].intime == -1) 
-        Heap[*heapsize].intime = *currentTime; 
-    ++(*heapsize); 
-  
-    // Ordering the Heap 
-    while (start != 0 && Heap[(start - 1) / 2].priority >  
-                                  Heap[start].priority) { 
-        Process temp = Heap[(start - 1) / 2]; 
-        Heap[(start - 1) / 2] = Heap[start]; 
-        Heap[start] = temp; 
-        start = (start - 1) / 2; 
+      // Sorting Queue[]
+      while (start != 0 && Queue[(start - 1) / 2].priority > Queue[start].priority)
+      { 
+          Process temp = Queue[(start - 1) / 2]; 
+          Queue[(start - 1) / 2] = Queue[start]; 
+          Queue[start] = temp; 
+          start = (start - 1) / 2; 
+      } 
     } 
-} 
+
+    // Sort Queue[] again after insertion of new processes
+    void sort(Process Queue[], int* queue_size, int start)  
+    { 
+      int smallest = start; 
+      int left = 2 * start + 1; 
+      int right = 2 * start + 2; 
+      if (left < *queue_size && Queue[left].priority < Queue[smallest].priority) 
+          smallest = left; 
+      if (right < *queue_size && Queue[right].priority < Queue[smallest].priority) 
+          smallest = right; 
   
-// It is used to reorder the heap according to 
-// priority if the processes after insertion 
-// of new process. 
-void order(Process Heap[], int* heapsize, int start) 
-{ 
-    int smallest = start; 
-    int left = 2 * start + 1; 
-    int right = 2 * start + 2; 
-    if (left < *heapsize && Heap[left].priority < 
-                            Heap[smallest].priority) 
-        smallest = left; 
-     if (right < *heapsize && Heap[right].priority <  
-                           Heap[smallest].priority) 
-        smallest = right; 
-  
-    // Ordering the Heap 
-    if (smallest != start) { 
-        Process temp = Heap[smallest]; 
-        Heap[smallest] = Heap[start]; 
-        Heap[start] = temp; 
-        order(Heap, heapsize, smallest); 
+      // Sorting the Queue
+      if (smallest != start)
+      { 
+          Process temp = Queue[smallest]; 
+          Queue[smallest] = Queue[start]; 
+          Queue[start] = temp; 
+          order(Queue, queue_size, smallest); 
+      } 
     } 
-} 
   
-// This function is used to find the process with 
-// highest priority from the heap. It also reorders 
-// the heap after extracting the highest priority process. 
-Process extractminimum(Process Heap[], int* heapsize, 
-                       int* currentTime) 
-{ 
-    Process min = Heap[0]; 
-    if (min.responsetime == -1) 
-        min.responsetime = *currentTime - min.arrivalTime; 
-    --(*heapsize); 
-    if (*heapsize >= 1) { 
-        Heap[0] = Heap[*heapsize]; 
-        order(Heap, heapsize, 0); 
+    // Find highest priority process and sort the Queue[] again
+    Process extractminimum(Process Queue[], int* queue_size, int* current_time) //
+    { 
+      Process min = Queue[0]; 
+      if (min.response_time == -1) 
+          min.response_time = *current_time - min.arrival_time; 
+    
+      --(*queue_size); 
+    
+      if (*queue_size >= 1)
+      { 
+          Queue[0] = Queue[*queue_size]; 
+          order(Queue, queue_size, 0); 
+      } 
+      return min;
     } 
-    return min; 
-} 
   
-// Compares two intervals according to staring times. 
-bool compare(Process p1, Process p2) 
-{ 
-    return (p1.arrivalTime < p2.arrivalTime); 
-} 
+    // Compares two intervals of processes according to staring times
+    bool compare(Process p1, Process p2) 
+    { 
+      return (p1.arrival_time < p2.arrival_time); 
+    } 
   
-// This function is responsible for executing 
-// the highest priority extracted from Heap[]. 
-void scheduling(Process Heap[], Process array[], int n, 
-                int* heapsize, int* currentTime) 
-{ 
-    if (heapsize == 0) 
+    // Execute the highest priority process from Queue[] 
+    void scheduling(Process Queue[], Process array[], int n, int* queue_size, int* current_time) 
+    { 
+      if (queue_size == 0) 
         return; 
   
-    Process min = extractminimum(Heap, heapsize, currentTime); 
-    min.outtime = *currentTime + 1; 
-    --min.burstTime; 
-    printf("process id = %d current time = %d\n", 
-           min.processID, *currentTime); 
+      Process min = extractminimum(Queue, queue_size, current_time); 
+      min.out_time = *current_time + 1; 
+      --min.burst_time;
+      cout << "Process ID = " << min.process_id << "Current Time = " << *current_time;
   
-    // If the process is not yet finished 
-    // insert it back into the Heap*/ 
-    if (min.burstTime > 0) { 
-        insert(Heap, min, heapsize, currentTime); 
-        return; 
-    } 
+      //Reinsert the unfinished process into the Queue[] 
+      if (min.burst_time > 0)
+      { 
+          insert(Queue, min, queue_size, current_time); 
+          return; 
+      } 
   
-    for (int i = 0; i < n; i++) 
-        if (array[i].processID == min.processID) { 
+      for (int i = 0; i < n; i++)
+      {
+          if (array[i].process_id == min.process_id)
+          {
             array[i] = min; 
             break; 
-        } 
-} 
+          }
+      }
+    } 
   
 // This function is responsible for 
 // managing the entire execution of the 
 // processes as they arrive in the CPU 
 // according to their arrival time. 
-void priority(Process array[], int n) 
-{ 
-    sort(array, array + n, compare); 
+    void priority(Process array[], int n) 
+    {
+      sort(array, array + n, compare); 
   
-    int totalwaitingtime = 0, totalbursttime = 0, 
-        totalturnaroundtime = 0, i, insertedprocess = 0, 
-        heapsize = 0, currentTime = array[0].arrivalTime, 
-        totalresponsetime = 0; 
+      int total_waiting_time = 0;
+      int total_burst_time = 0;
+      int total_turnaround_time = 0;
+      int inserted_process = 0; 
+      int queue_size = 0;
+      int current_time = array[0].arrival_time; 
+      int total_response_time = 0;
   
-    Process Heap[4 * n]; 
+      Process Queue[4 * n]; 
   
-    // Calculating the total burst time 
-    // of the processes 
-    for (int i = 0; i < n; i++) { 
-        totalbursttime += array[i].burstTime; 
-        array[i].tempburstTime = array[i].burstTime; 
-    } 
+      // Calculating the total burst time of the processes 
+      for (int i = 0; i < n; i++)
+      { 
+          total_burst_time += array[i].burst_time; 
+          array[i].temp_burst_time = array[i].burst_time; 
+      } 
   
-    // Inserting the processes in Heap 
-    // according to arrival time 
-    do { 
-        if (insertedprocess != n) { 
-            for (i = 0; i < n; i++) { 
-                if (array[i].arrivalTime == currentTime) { 
-                    ++insertedprocess; 
-                    array[i].intime = -1; 
-                    array[i].responsetime = -1; 
-                    insert(Heap, array[i], &heapsize, ¤tTime); 
-                } 
+      // Inserting the processes in Heap 
+      // according to arrival time 
+      do{
+        if (inserted_process != n)
+        {
+          for (i = 0; i < n; i++)
+          {
+            if (array[i].arrival_time == current_time)
+            {
+              ++inserted_process; 
+              array[i].in_time = -1; 
+              array[i].response_time = -1; 
+              insert(Queue, array[i], &heap_size, current_time); 
             } 
+          } 
         } 
-        scheduling(Heap, array, n, &heapsize, ¤tTime); 
-        ++currentTime; 
-        if (heapsize == 0 && insertedprocess == n) 
-            break; 
-    } while (1); 
+        scheduling(Queue, array, n, &queue_size, current_time); 
+        ++current_time; 
+        if (queue_size == 0 && inserted_process == n) 
+            break;
+      }while (1); 
   
-    for (int i = 0; i < n; i++) { 
-        totalresponsetime += array[i].responsetime; 
-        totalwaitingtime += (array[i].outtime - array[i].intime - 
-                                         array[i].tempburstTime); 
-        totalbursttime += array[i].burstTime; 
-    } 
-    printf("Average waiting time = %f\n", 
-           ((float)totalwaitingtime / (float)n)); 
-    printf("Average response time =%f\n", 
-           ((float)totalresponsetime / (float)n)); 
-    printf("Average turn around time = %f\n", 
-           ((float)(totalwaitingtime + totalbursttime) / (float)n)); 
-} 
-  
-// Driver code 
+      for (int i = 0; i < n; i++)
+      {
+        total_response_time += array[i].response_time; 
+        total_waiting_time += (array[i].out_time - array[i].in_time - array[i].temp_burst_time); 
+        total_burst_time += array[i].burst_time; 
+      }
+      
+      cout << "Average waiting time \n" << (float)totalwaitingtime / (float)n; 
+      cout << "Average response time \n" << (float)totalresponsetime / (float)n; 
+      cout << "Average turn around time \n" << (float)(totalwaitingtime + totalbursttime) / (float)n; 
+    }
+}
+
 int main() 
-{ 
-    int n, i; 
-    Process a[5]; 
-    a[0].processID = 1; 
-    a[0].arrivalTime = 4; 
-    a[0].priority = 2; 
-    a[0].burstTime = 6; 
-    a[1].processID = 4; 
-    a[1].arrivalTime = 5; 
-    a[1].priority = 1; 
-    a[1].burstTime = 3; 
-    a[2].processID = 2; 
-    a[2].arrivalTime = 5; 
-    a[2].priority = 3; 
-    a[3].burstTime = 7; 
-    a[3].processID = 3; 
-    a[3].arrivalTime = 1; 
-    a[3].priority = 4; 
-    a[3].burstTime = 2; 
-    a[4].processID = 5; 
-    a[4].arrivalTime = 3; 
-    a[4].priority = 5; 
-    a[4].burstTime = 4; 
-    priority(a, 5); 
-    return 0; 
+{
+  int n, process_id, priority, arrival_time, burst_time;
+  cout << "Enter no. of processes: ";
+  cin >> n;
+  
+  Process process[n];
+  cout << "Enter Process ID, Priority, Arrival Time & Burst Time:";
+  for(int i = 0; i < n; i++)
+  {
+    cout << i;
+    cin >> process_id >> priority >> arrival_time >> burst_time;
+    process[i].process_id = process_id;
+    process[i].priority = priority;
+    process[i].arrival_time = arrival_time;
+    process[i].burst_time = burst_time;
+  }
+  
+  priority(process, n);
+  
+  return 0; 
 } 
